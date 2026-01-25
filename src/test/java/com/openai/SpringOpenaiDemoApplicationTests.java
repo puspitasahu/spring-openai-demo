080535d0c6bad4e85ce0b1cfe95ce827e7835184
puspitasahu <puspita5sahu@gmail.com>
2026-01-25 12:20:22 +0530

    updated docker compose file

--- src/test/java/com/openai/SpringOpenaiDemoApplicationTests.java
+++ src/test/java/com/openai/SpringOpenaiDemoApplicationTests.java
@@ -3,11 +3,11 @@ package com.openai;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringOpenaiDemoApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
